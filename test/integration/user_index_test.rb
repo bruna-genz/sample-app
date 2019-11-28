043d476956b3e4ddd59b954bddffaa538d769eed
Bruna <brunagenz92@gmail.com>
2019-11-28 11:33:34 -0400

    fix bug and got the tests green

--- test/integration/user_index_test.rb
+++ test/integration/user_index_test.rb
@@ -4,6 +4,7 @@ class UserIndexTest < ActionDispatch::IntegrationTest
   
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "index as admin including pagination and delete links" do
@@ -11,6 +12,9 @@ class UserIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'nav.pagination'
+    
+    # TO-DO: fix pagination to make this test work 
+
     # first_page_of_users = User.paginate(page: 1)
     # first_page_of_users.each do |user|
     #   assert_select 'a[href=?]', user_path(user), text: user.name
@@ -18,13 +22,14 @@ class UserIndexTest < ActionDispatch::IntegrationTest
     #    assert_select 'a[href=?]', user_path(user), text: 'delete'
     #  end
     # end
+
     assert_difference 'User.count', -1 do
-      delete user_path(@non_admin)
+      delete user_path(@other_user)
     end
   end
 
   test "index as non-admin" do
-    log_in_as(@non_admin)
+    log_in_as(@other_user)
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
