aa73f2e251118b99549c53d5c594fe6dbe7401d8
Bruna <brunagenz92@gmail.com>
2019-11-29 11:09:04 -0400

    generate password_resets_controller with actions new and edit; add resource routes for password reset; update session's new.html.erb to include a link to password reset.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://lit-garden-22323.herokuapp.com'
+  host = 'lit-garden-22323.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
